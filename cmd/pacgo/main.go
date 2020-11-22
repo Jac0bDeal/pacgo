1c4006c1fbb95fd7217390312c5ee6e81a52430e
Jacob Deal <jacob.r.deal@gmail.com>
2020-11-22 02:11:42 -0500

    add ability to read from stdin in cbreak mode

--- cmd/pacgo/main.go
+++ cmd/pacgo/main.go
@@ -2,19 +2,47 @@ package main
 
 import (
 	"log"
+	"os"
+	"os/exec"
 
 	"github.com/Jac0bDeal/pacgo/internal/pacgo"
 )
 
 func main() {
+	//initialize game
+	initialize()
+	defer cleanup()
+
+	// load resources
 	maze, err := pacgo.LoadMaze("maze.txt")
 	if err != nil {
 		log.Fatalf("failed to load maze from file: %v", err)
 	}
 
 	for {
+		// update screen
 		maze.PrintScreen()
 
 		break
+	}
+}
+
+func initialize() {
+	cbTerm := exec.Command("stty", "cbreak", "-echo")
+	cbTerm.Stdin = os.Stdin
+
+	err := cbTerm.Run()
+	if err != nil {
+		log.Fatalf("unable to activate cbreak mode: %v", err)
+	}
+}
+
+func cleanup() {
+	cookedTerm := exec.Command("stty", "-cbreak", "echo")
+	cookedTerm.Stdin = os.Stdin
+
+	err := cookedTerm.Run()
+	if err != nil {
+		log.Fatalf("unable to restore cooked mode: %v", err)
 	}
 }
