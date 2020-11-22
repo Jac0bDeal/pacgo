2e6ab9fc53625e8c770f1ec105515b5d275bebc7
Jacob Deal <jacob.r.deal@gmail.com>
2020-11-22 02:01:48 -0500

    add basic maze

--- cmd/pacgo/main.go
+++ cmd/pacgo/main.go
@@ -1,5 +1,20 @@
 package main
 
-func main() {
+import (
+	"log"
 
+	"github.com/Jac0bDeal/pacgo/internal/pacgo"
+)
+
+func main() {
+	maze, err := pacgo.LoadMaze("maze.txt")
+	if err != nil {
+		log.Fatalf("failed to load maze from file: %v", err)
+	}
+
+	for {
+		maze.PrintScreen()
+
+		break
+	}
 }
