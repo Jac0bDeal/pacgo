bafe725a45900d87f5e6771eee25927a4b449740
Jacob Deal <jacob.r.deal@gmail.com>
2020-12-10 01:59:31 -0500

    parse config and level file from command line args

--- cmd/pacgo/main.go
+++ cmd/pacgo/main.go
@@ -1,13 +1,23 @@
 package main
 
 import (
+	"flag"
 	"log"
 
 	"github.com/Jac0bDeal/pacgo/internal/pacgo"
 )
 
+var (
+	configFile = flag.String("config-file", "configs/config-emoji.json", "path to config file")
+	levelFile  = flag.String("level-file", "levels/level-1.txt", "path to level file")
+)
+
+func init() {
+	flag.Parse()
+}
+
 func main() {
-	app, err := pacgo.New("configs/config-emoji.json", "levels/level-1.txt")
+	app, err := pacgo.New(*configFile, *levelFile)
 	if err != nil {
 		log.Fatalf("failed to initialize app: %v", err)
 	}
